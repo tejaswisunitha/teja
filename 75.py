06264a3776d9db3feb02378d1b541f1ec420bee0
tejaswini kommanaboina <42573830+tejaswisunitha@users.noreply.github.com>
2018-12-05 11:38:30 +0530

    Update 75.py

--- 75.py
+++ 75.py
@@ -1,11 +1,11 @@
 ss=raw_input()
-c=len(ss)
+b=len(ss)
 a=list(ss)
-if c%2==0:
-    m=c/2 - 1
+if b%2==0:
+    m=b/2 - 1
     a[m]='*'
     a[m+1]='*'
 else:
-    m=c/2 - 1
+    m=b/2 - 1
     a[m+1]='*'
 print "".join(a)
