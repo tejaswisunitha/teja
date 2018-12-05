243aa2f671580fe1024cac91225e555637fec4d6
tejaswini kommanaboina <42573830+tejaswisunitha@users.noreply.github.com>
2018-12-05 11:46:00 +0530

    Update 76.py

--- 76.py
+++ 76.py
@@ -1,8 +1,8 @@
-n=int(raw_input())
+m=int(raw_input())
 count=0
-if n>1:
-    for i in range(2,n):
-        if n%i==0:
+if m>1:
+    for i in range(2,m):
+        if m%i==0:
             count=count+1
 if count>1:
     print "yes"
