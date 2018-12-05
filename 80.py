feb2074e3c7b6620e2e9a33425b6ec6f1fe74275
tejaswini kommanaboina <42573830+tejaswisunitha@users.noreply.github.com>
2018-12-05 12:35:38 +0530

    Update 80.py

--- 80.py
+++ 80.py
@@ -1,8 +1,8 @@
 a=int(raw_input())
 b=[]
 while a!=0:
-    c=a%10
-    if c%2==1:
-        b.append(c)
+    d=a%10
+    if d%2==1:
+        b.append(d)
     a=a/10
 print str(b[::-1]).replace('[',"").replace(']',"")
