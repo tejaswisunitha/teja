93e5b96bb0979e0af278ceac8da3bbb7ffff045f
tejaswini kommanaboina <42573830+tejaswisunitha@users.noreply.github.com>
2018-12-06 12:25:15 +0530

    Update 87.py

--- 87.py
+++ 87.py
@@ -1,8 +1,8 @@
-a,b=map(int,raw_input().split())
+c,d=map(int,raw_input().split())
 def gcd(p,q):
     s=abs(p-q)
     if (p-q)==0:
         return q
     else:
         return gcd(s,min(p,q))
-print gcd(a,b)
+print gcd(c,d)
