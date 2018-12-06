34fdf9e493a364005b558848fe0604e3b1e5b6b3
tejaswini kommanaboina <42573830+tejaswisunitha@users.noreply.github.com>
2018-12-06 12:16:53 +0530

    Create 83.py

---
+++ 83.py
@@ -0,0 +1,12 @@
+a1=int(raw_input())
+b1=int(raw_input())
+a2=int(raw_input())
+b2=int(raw_input())
+a3=int(raw_input())
+b3=int(raw_input())
+a4=int(raw_input())
+b4=int(raw_input())
+print a1 / b1
+print a2 % b2
+print a3 / b3
+print a4 % b4
