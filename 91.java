5db8e0edfe78541960bc1d75157bc695ee9628bf
tejaswini kommanaboina <42573830+tejaswisunitha@users.noreply.github.com>
2018-12-07 10:32:05 +0530

    Create 91.java

---
+++ 91.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+import java.lang.*;
+
+import java.io.*;
+
+
+class Ideone
+
+{
+	
+public static void main (String[] args) throws java.lang.Exception
+	
+{
+	
+int l,b,h,suface_area,volume;
+	
+Scanner s=new Scanner(System.in);
+	
+l=s.nextInt();
+	b=s.nextInt();
+	
+h=s.nextInt();
+	
+suface_area=(2*l*h)+(2*l*b)+(2*b*h);
+	
+volume=l*b*h;
+	
+System.out.print(suface_area);
+	
+
+System.out.print("\n"+volume);
+	
+}
+
+}
