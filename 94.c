777a0e37210cc83365c8bd2ed605b25baec5da5f
tejaswini kommanaboina <42573830+tejaswisunitha@users.noreply.github.com>
2018-12-07 11:58:24 +0530

    Update 94.c

--- 94.c
+++ 94.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 int main()
 {
-	int n,k,i,a[10];
-	scanf("%d\t%d",&n,&k);
+	int n,y,i,a[10];
+	scanf("%d\t%d",&n,&y);
 	for(i=0;i<=n;i++)
 	{
 	scanf("\n%d\t",&a[i]);
 	}
-	printf("%d",a[k-1]);
+	printf("%d",a[y-1]);
 	return 0;
 }
